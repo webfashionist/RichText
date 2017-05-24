716a0e96eb1a51e5b83e56424ffa6a40479dae30
Bob Schockweiler <info@webfashion.in>
2017-05-24 04:31:40 +0200

    Add basic functions

--- src/jquery.richtext.js
+++ src/jquery.richtext.js
@@ -2,28 +2,6 @@
  
     $.fn.richText = function( options ) {
 
-        var $inputElement = $(this);
-        var $editor,
-            $toolbarList = $('<ul />'),
-            $toolbarElement = $('<li />'),
-            $btnBold = $('<a />', {class: "richText-btn fa fa-bold"}), // bold
-            $btnItalic = $('<a />', {class: "richText-btn fa fa-italic"}), // italic
-            $btnUnderline = $('<a />', {class: "richText-btn fa fa-underline"}), // underline
-            $btnLeftAlign = $('<a />', {class: "richText-btn fa fa-align-left"}), // left align
-            $btnCenterAlign = $('<a />', {class: "richText-btn fa fa-align-center"}), // centered
-            $btnRightAlign = $('<a />', {class: "richText-btn fa fa-align-right"}), // right align
-            $btnOL = $('<a />', {class: "richText-btn fa fa-list-ol"}), // ordered list
-            $btnUL = $('<a />', {class: "richText-btn fa fa-list"}), // unordered list
-            $btnHeading = $('<a />', {class: "richText-btn fa fa-header"}), // title/header
-            $btnFontColor = $('<a />', {class: "richText-btn fa fa-paint-brush"}), // font color
-            $btnImageUpload = $('<a />', {class: "richText-btn fa fa-image"}), // image
-            $btnFileUpload = $('<a />', {class: "richText-btn fa fa-file-text-o"}), // file
-            $btnURLs = $('<a />', {class: "richText-btn fa fa-link"}), // urls/links
-            $btnTable = $('<a />', {class: "richText-btn fa fa-table"}), // table
-            $btnRemoveStyles = $('<a />', {class: "richText-btn fa fa-recycle"}), // clean up styles
-            $btnCode = $('<a />', {class: "richText-btn fa fa-code"}); // code
-
- 
         // set default options
         // and merge them with the parameter options
         var settings = $.extend({
@@ -65,7 +43,68 @@
         }, options );
 
 
-        // initizalize editor
+        /* prepare toolbar */
+        var $inputElement = $(this);
+        $inputElement.addClass("richText-initial");
+        var $editor,
+            $toolbarList = $('<ul />'),
+            $toolbarElement = $('<li />'),
+            $btnBold = $('<a />', {class: "richText-btn fa fa-bold"}), // bold
+            $btnItalic = $('<a />', {class: "richText-btn fa fa-italic"}), // italic
+            $btnUnderline = $('<a />', {class: "richText-btn fa fa-underline"}), // underline
+            $btnLeftAlign = $('<a />', {class: "richText-btn fa fa-align-left"}), // left align
+            $btnCenterAlign = $('<a />', {class: "richText-btn fa fa-align-center"}), // centered
+            $btnRightAlign = $('<a />', {class: "richText-btn fa fa-align-right"}), // right align
+            $btnOL = $('<a />', {class: "richText-btn fa fa-list-ol"}), // ordered list
+            $btnUL = $('<a />', {class: "richText-btn fa fa-list"}), // unordered list
+            $btnHeading = $('<a />', {class: "richText-btn fa fa-header"}), // title/header
+            $btnFontColor = $('<a />', {class: "richText-btn fa fa-paint-brush"}), // font color
+            $btnImageUpload = $('<a />', {class: "richText-btn fa fa-image"}), // image
+            $btnFileUpload = $('<a />', {class: "richText-btn fa fa-file-text-o"}), // file
+            $btnURLs = $('<a />', {class: "richText-btn fa fa-link"}), // urls/links
+            $btnTable = $('<a />', {class: "richText-btn fa fa-table"}), // table
+            $btnRemoveStyles = $('<a />', {class: "richText-btn fa fa-recycle"}), // clean up styles
+            $btnCode = $('<a />', {class: "richText-btn fa fa-code"}); // code
+
+        
+        /* prepare toolbar dropdowns */
+        var $dropdownOuter = $('<div />', {class: "richText-dropdown-outer"});
+        var $dropdownList = $('<ul />', {class: "richText-dropdown"}), // dropdown lists
+            $dropdownBox = $('<div />', {class: "richText-dropdown"}), // dropdown boxes / custom dropdowns
+            $form = $('<div />', {class: "richText-form"}), // symbolic form
+            $formItem = $('<div />', {class: 'richText-form-item'}), // form item
+            $formLabel = $('<label />'), // form label
+            $formInput = $('<input />', {type: "text"}), //form input field
+            $formInputFile = $('<input />', {type: "file"}), // form file input field
+            $formButton = $('<button />', {text: "Einfügen", class: "btn"}); // button
+
+
+        /* list dropdown for titles */
+        $titles = $dropdownList.clone();
+        $titles.append($('<li />', {html: '<a data-title="1">Title #1</a>'}));
+        $titles.append($('<li />', {html: '<a data-title="2">Title #2</a>'}));
+        $titles.append($('<li />', {html: '<a data-title="3">Title #3</a>'}));
+        $titles.append($('<li />', {html: '<a data-title="4">Title #4</a>'}));
+        $btnHeading.append($dropdownOuter.clone().append($titles));
+
+        /* box dropdown for links */
+        $linksDropdown = $dropdownBox.clone();
+        $linksForm = $form.clone();
+        $linksForm.append(
+            $formItem.clone()
+                .append($formLabel.clone().text("URL").attr("for", "url"))
+                .append($formInput.clone().attr("id", "url"))
+               );
+        $linksForm.append(
+            $formItem.clone()
+                .append($formLabel.clone().text("Text").attr("for", "urlText"))
+                .append($formInput.clone().attr("id", "urlText"))
+               );
+        $linksForm.append( $formItem.clone().append($formButton) );
+        $linksDropdown.append($linksForm);
+        $btnURLs.append($dropdownOuter.clone().append($linksDropdown));
+
+        /* initizalize editor */
         var init = function() {
             $editor = $('<div />', {class: "richText"});
             var $toolbar = $('<div />', {class: "richText-toolbar"});
@@ -141,6 +180,46 @@
         };
 
         init();
+
+
+        /**
+         * Toolbar button actions
+         */
+        $(document).on("click", ".richText-btn", function(event) {
+            event.preventDefault();
+
+            if($(this).hasClass("fa-code")) {
+                // toggle displaying code
+                __toggleCode();
+            }
+        });
+
+
+        /**
+         * Toggle (show/hide) code or editor
+         * @return {void}
+         */
+        function __toggleCode() {
+
+            if($editor.find('.richText-editor').is(":visible")) {
+                // show code
+                $editor.find('.richText-initial').show();
+                $editor.find('.richText-editor').hide(); 
+                // disable non working buttons
+                $('.richText-toolbar').find('.richText-btn').each(function() {
+                    if(!$(this).hasClass("fa-code")) {
+                        $(this).parent('li').attr("data-disable", "true");
+                    }
+                });
+            } else {
+                // show editor
+                $editor.find('.richText-initial').hide();
+                $editor.find('.richText-editor').show();
+                // enable all buttons again
+                $('.richText-toolbar').find('li').removeAttr("data-disable");
+            }
+
+        }
 
         return $(this);
     };
