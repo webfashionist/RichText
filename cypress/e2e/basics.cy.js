4f532d2e1a4d57f369970f85ad900143698ff8f8
Bob Schockweiler <info@webfashion.in>
2026-06-24 17:05:37 +0200

    feat: Update cypress e2e tests for font-size

--- cypress/e2e/basics.cy.js
+++ cypress/e2e/basics.cy.js
@@ -1,198 +1,230 @@
 describe('basic formatting', () => {
-  const editorSelector = '.richText-editor';
-  const commandSelector = (command, options) => {
-    const selector = `a[data-command="${command}"]`;
-    if (options) {
-      return `${selector}[data-option="${options}"]`;
-    }
-    return selector;
-  };
+    const editorSelector = '.richText-editor';
+    const commandSelector = (command, options) => {
+        const selector = `a[data-command="${command}"]`;
+        if (options) {
+            return `${selector}[data-option="${options}"]`;
+        }
+        return selector;
+    };
 
-  const typeWithCommand = (command, text, options) => {
-      cy.get(commandSelector(command, options)).click();
-      cy.get(editorSelector).type(text);
-  };
+    const typeWithCommand = (command, text, options) => {
+        cy.get(commandSelector(command, options)).click();
+        cy.get(editorSelector).type(text);
+    };
 
-  beforeEach(() => {
-    cy.visit('/examples/index.html');
-    cy.get(editorSelector).focus();
-  });
+    beforeEach(() => {
+        cy.visit('/examples/index.html');
+        cy.get(editorSelector).focus();
+    });
 
-  it('has correct init state', () => {
-    cy.get(editorSelector).should('have.html', '<div><br></div>');
-  });
+    it('has correct init state', () => {
+        cy.get(editorSelector).should('have.html', '<div><br></div>');
+    });
 
-  it('writes text unstyled by default', () => {
-    cy.get(editorSelector).type('hello world');
-    cy.get(editorSelector).should('have.html', '<div>hello world</div>');
-  });
+    it('writes text unstyled by default', () => {
+        cy.get(editorSelector).type('hello world');
+        cy.get(editorSelector).should('have.html', '<div>hello world</div>');
+    });
 
-  it('can write text in bold', () => {
-    typeWithCommand('bold', 'hello world');
-    cy.get(editorSelector).should('have.html', '<div><b>hello world</b></div>');
-  });
+    it('can write text in bold', () => {
+        typeWithCommand('bold', 'hello world');
+        cy.get(editorSelector).should('have.html', '<div><b>hello world</b></div>');
+    });
 
-  it('can write text in italic', () => {
-    typeWithCommand('italic', 'hello world');
-    cy.get(editorSelector).should('have.html', '<div><i>hello world</i></div>');
-  });
+    it('can write text in italic', () => {
+        typeWithCommand('italic', 'hello world');
+        cy.get(editorSelector).should('have.html', '<div><i>hello world</i></div>');
+    });
 
-  it('can write text in underline', () => {
-    typeWithCommand('underline', 'hello world');
-    cy.get(editorSelector).should('have.html', '<div><u>hello world</u></div>');
-  });
+    it('can write text in underline', () => {
+        typeWithCommand('underline', 'hello world');
+        cy.get(editorSelector).should('have.html', '<div><u>hello world</u></div>');
+    });
 
-  it('can write text left-aligned', () => {
-    cy.get(editorSelector).type('hello world');
-    cy.get(commandSelector('justifyRight')).click();
-    cy.get(commandSelector('justifyLeft')).click();
-    cy.get(editorSelector).should('have.html', '<div style="text-align: left;">hello world</div>');
-  });
+    it('can write text left-aligned', () => {
+        cy.get(editorSelector).type('hello world');
+        cy.get(commandSelector('justifyRight')).click();
+        cy.get(commandSelector('justifyLeft')).click();
+        cy.get(editorSelector).should('have.html', '<div style="text-align: left;">hello world</div>');
+    });
 
-  it('can write text center-aligned', () => {
-    cy.get(editorSelector).type('hello world');
-    cy.get(commandSelector('justifyCenter')).click();
-    cy.get(editorSelector).should('have.html', '<div style="text-align: center;">hello world</div>');
-  });
+    it('can write text center-aligned', () => {
+        cy.get(editorSelector).type('hello world');
+        cy.get(commandSelector('justifyCenter')).click();
+        cy.get(editorSelector).should('have.html', '<div style="text-align: center;">hello world</div>');
+    });
 
-  it('can write text right-aligned', () => {
-    cy.get(editorSelector).type('hello world');
-    cy.get(commandSelector('justifyRight')).click();
-    cy.get(editorSelector).should('have.html', '<div style="text-align: right;">hello world</div>');
-  });
+    it('can write text right-aligned', () => {
+        cy.get(editorSelector).type('hello world');
+        cy.get(commandSelector('justifyRight')).click();
+        cy.get(editorSelector).should('have.html', '<div style="text-align: right;">hello world</div>');
+    });
 
-  it('can write text fully-aligned', () => {
-    cy.get(editorSelector).type('hello world');
-    cy.get(commandSelector('justifyFull')).click();
-    cy.get(editorSelector).should('have.html', '<div style="text-align: justify;">hello world</div>');
-  });
+    it('can write text fully-aligned', () => {
+        cy.get(editorSelector).type('hello world');
+        cy.get(commandSelector('justifyFull')).click();
+        cy.get(editorSelector).should('have.html', '<div style="text-align: justify;">hello world</div>');
+    });
 
-  it('can write ordered lists', () => {
-    typeWithCommand('insertOrderedList', 'hello world');
-    cy
-      .get(editorSelector)
-      .should('have.html', '<div><ol><li>hello world</li></ol></div>')
-      .type('{enter}')
-      .type('hello world2')
-      .get(editorSelector)
-      .should('have.html', '<div><ol><li>hello world</li><li>hello world2</li></ol></div>');
-  });
-
-  it('can write unorderd lists', () => {
-    typeWithCommand('insertUnorderedList', 'hello world');
-    cy
-      .get(editorSelector)
-      .should('have.html', '<div><ul><li>hello world</li></ul></div>')
-      .type('{enter}')
-      .type('hello world2')
-      .get(editorSelector)
-      .should('have.html', '<div><ul><li>hello world</li><li>hello world2</li></ul></div>');
-  });
-
-  it('can remove formatting', () => {
-      // check bold
-    typeWithCommand('bold', 'hello world');
-    cy.get(editorSelector).should('have.html', '<div><b>hello world</b></div>');
-    cy.get(editorSelector).type('{selectall}');
-    cy.get(commandSelector('removeFormat')).click();
-    cy.get(editorSelector).should('have.html', '<div>hello world</div>');
-    // check italic
-    cy.get(editorSelector).type('{selectall}');
-    typeWithCommand('italic', 'hello world');
-    cy.get(editorSelector).should('have.html', '<div><i>hello world</i></div>');
-    cy.get(editorSelector).type('{selectall}');
-    cy.get(commandSelector('removeFormat')).click();
-    cy.get(editorSelector).should('have.html', '<div>hello world</div>');
-  });
-
-  it('can write text with font-size', () => {
-    for (let n = 24; n >= 12; n -= 2) {
-      cy.get(editorSelector).type('{selectall}');
-      cy.get(commandSelector('removeFormat')).click();
-      cy.get(editorSelector).type(`hello world ${n}`);
-      cy.get(editorSelector).type('{selectall}');
-      const buttonSelector = cy.get(`.fa-text-height`).parent(`a`);
-      buttonSelector.click(); // open dropdown
-      // check if dropdown is open
-      buttonSelector.children(`.richText-dropdown-outer`).should('be.visible');
-
-      cy.get(`a[data-command="fontSize"][data-option="${n}"]`).click();
-      cy
-          .get(editorSelector)
-          .should('include.html', `<div><span style="font-size:${n}px;">hello world ${n}</span></div>`)
-          .type('{enter}');
-    }
-  });
-
-  it('can write text with font-family', () => {
-    const fontNames = [
-        'Arial',
-        'Arial Black',
-        'Comic Sans MS',
-        'Courier New',
-        'Geneva',
-        'Georgia',
-        'Helvetica',
-        'Impact',
-        'Lucida Console',
-        'Tahoma',
-        'Times New Roman',
-        'Verdana'
-    ];
-
-    for (const fontName of fontNames) {
-      cy.get(editorSelector).type('{selectall}');
-      cy.get(commandSelector('removeFormat')).click();
-      cy.get(editorSelector).type(`hello world ${fontName}`);
-      cy.get(editorSelector).type('{selectall}');
-      const buttonSelector = cy.get(`.fa-font`).parent(`a`);
-      buttonSelector.click(); // open dropdown
-      // check if dropdown is open
-      buttonSelector.children(`.richText-dropdown-outer`).should('be.visible');
-
-      cy.get(`a[data-command="fontName"][data-option="${fontName}"]`).click();
-      cy
-          .get(editorSelector)
-          .should('include.html', `<div><font face="${fontName}">hello world ${fontName}</font></div>`)
-          .type('{enter}');
-    }
-  });
-
-  it('can write text with font color', () => {
-    const fontColors = [
-        '#FFFFFF',
-        '#000000',
-        '#7F6000',
-        '#938953',
-        '#1F497D',
-        '#4F81BD',
-        '#953734',
-        '#4F6128',
-        '#3F3151',
-        '#31859B',
-        '#4BACC6',
-        '#E36C09',
-        '#F79646',
-        '#FFFF00'
-    ];
-
-    for (const fontColor of fontColors) {
-        cy.get(editorSelector).type('{selectall}');
-        cy.get(commandSelector('removeFormat')).click();
-        cy.get(editorSelector).type(`hello world ${fontColor}`);
-        cy.get(editorSelector).type('{selectall}');
-        const buttonSelector = cy.get(`.fa-pencil`).parent(`a`);
-        buttonSelector.click(); // open dropdown
-        // check if dropdown is open
-        buttonSelector.children(`.richText-dropdown-outer`).should('be.visible');
-
-        cy.get(`a[data-command="forecolor"][data-option="${fontColor}"]`).click();
+    it('can write ordered lists', () => {
+        typeWithCommand('insertOrderedList', 'hello world');
         cy
             .get(editorSelector)
-            .should('include.html', `<div><font color="${fontColor.toLowerCase()}">hello world ${fontColor}</font></div>`)
+            .should('have.html', '<div><ol><li>hello world</li></ol></div>')
+            .type('{enter}')
+            .type('hello world2')
+            .get(editorSelector)
+            .should('have.html', '<div><ol><li>hello world</li><li>hello world2</li></ol></div>');
+    });
+
+    it('can write unorderd lists', () => {
+        typeWithCommand('insertUnorderedList', 'hello world');
+        cy
+            .get(editorSelector)
+            .should('have.html', '<div><ul><li>hello world</li></ul></div>')
+            .type('{enter}')
+            .type('hello world2')
+            .get(editorSelector)
+            .should('have.html', '<div><ul><li>hello world</li><li>hello world2</li></ul></div>');
+    });
+
+    it('can remove formatting', () => {
+        // check bold
+        typeWithCommand('bold', 'hello world');
+        cy.get(editorSelector).should('have.html', '<div><b>hello world</b></div>');
+        cy.get(editorSelector).type('{selectall}');
+        cy.get(commandSelector('removeFormat')).click();
+        cy.get(editorSelector).should('have.html', '<div>hello world</div>');
+        // check italic
+        cy.get(editorSelector).type('{selectall}');
+        typeWithCommand('italic', 'hello world');
+        cy.get(editorSelector).should('have.html', '<div><i>hello world</i></div>');
+        cy.get(editorSelector).type('{selectall}');
+        cy.get(commandSelector('removeFormat')).click();
+        cy.get(editorSelector).should('have.html', '<div>hello world</div>');
+    });
+
+    it('can write text with font-size', () => {
+        for (let n = 24; n >= 12; n -= 2) {
+            cy.get(editorSelector).type('{selectall}');
+            cy.get(commandSelector('removeFormat')).click();
+            cy.get(editorSelector).type(`hello world ${n}`);
+            cy.get(editorSelector).type('{selectall}');
+            const buttonSelector = cy.get(`.fa-text-height`).parent(`a`);
+            buttonSelector.click(); // open dropdown
+            // check if dropdown is open
+            buttonSelector.children(`.richText-dropdown-outer`).should('be.visible');
+
+            cy.get(`a[data-command="fontSize"][data-option="${n}"]`).click();
+            cy
+                .get(editorSelector)
+                .should('include.html', `<div><span style="font-size:${n}px;">hello world ${n}</span></div>`)
+                .type('{enter}');
+        }
+    });
+
+    it('can write text with multiple selected font-size', () => {
+        cy.get(editorSelector).type('{selectall}');
+        cy.get(commandSelector('removeFormat')).click();
+        cy.get(editorSelector).type(`hello world 12`);
+        cy.get(editorSelector).type('{selectall}');
+        const buttonSelector = cy.get(`.fa-text-height`).parent(`a`);
+        buttonSelector.click(); // open dropdown
+
+        cy.get(`a[data-command="fontSize"][data-option="12"]`).click();
+        cy
+            .get(editorSelector)
+            .should('include.html', `<div><span style="font-size:12px;">hello world 12</span></div>`);
+
+        cy.get(editorSelector).type('{selectall}');
+        buttonSelector.click(); // open dropdown
+
+        cy.get(`a[data-command="fontSize"][data-option="24"]`).click();
+        cy
+            .get(editorSelector)
+            .should('include.html', `<div><span><span style="font-size:24px;">hello world 12</span></span></div>`)
             .type('{enter}');
-    }
-  });
+
+        cy.get(editorSelector).type('{selectall}');
+        buttonSelector.click(); // open dropdown
+
+        cy.get(`a[data-command="fontSize"][data-option="18"]`).click();
+        cy
+            .get(editorSelector)
+            .should('include.html', `<div><span><span><span style="font-size:18px;">hello world 12</span></span></span></div>`)
+            .type('{enter}');
+    });
+
+    it('can write text with font-family', () => {
+        const fontNames = [
+            'Arial',
+            'Arial Black',
+            'Comic Sans MS',
+            'Courier New',
+            'Geneva',
+            'Georgia',
+            'Helvetica',
+            'Impact',
+            'Lucida Console',
+            'Tahoma',
+            'Times New Roman',
+            'Verdana'
+        ];
+
+        for (const fontName of fontNames) {
+            cy.get(editorSelector).type('{selectall}');
+            cy.get(commandSelector('removeFormat')).click();
+            cy.get(editorSelector).type(`hello world ${fontName}`);
+            cy.get(editorSelector).type('{selectall}');
+            const buttonSelector = cy.get(`.fa-font`).parent(`a`);
+            buttonSelector.click(); // open dropdown
+            // check if dropdown is open
+            buttonSelector.children(`.richText-dropdown-outer`).should('be.visible');
+
+            cy.get(`a[data-command="fontName"][data-option="${fontName}"]`).click();
+            cy
+                .get(editorSelector)
+                .should('include.html', `<div><font face="${fontName}">hello world ${fontName}</font></div>`)
+                .type('{enter}');
+        }
+    });
+
+    it('can write text with font color', () => {
+        const fontColors = [
+            '#FFFFFF',
+            '#000000',
+            '#7F6000',
+            '#938953',
+            '#1F497D',
+            '#4F81BD',
+            '#953734',
+            '#4F6128',
+            '#3F3151',
+            '#31859B',
+            '#4BACC6',
+            '#E36C09',
+            '#F79646',
+            '#FFFF00'
+        ];
+
+        for (const fontColor of fontColors) {
+            cy.get(editorSelector).type('{selectall}');
+            cy.get(commandSelector('removeFormat')).click();
+            cy.get(editorSelector).type(`hello world ${fontColor}`);
+            cy.get(editorSelector).type('{selectall}');
+            const buttonSelector = cy.get(`.fa-pencil`).parent(`a`);
+            buttonSelector.click(); // open dropdown
+            // check if dropdown is open
+            buttonSelector.children(`.richText-dropdown-outer`).should('be.visible');
+
+            cy.get(`a[data-command="forecolor"][data-option="${fontColor}"]`).click();
+            cy
+                .get(editorSelector)
+                .should('include.html', `<div><font color="${fontColor.toLowerCase()}">hello world ${fontColor}</font></div>`)
+                .type('{enter}');
+        }
+    });
 
     it('can write text with background color', () => {
         const backgroundColors = [
@@ -244,14 +276,14 @@ describe('basic formatting', () => {
         }
     });
 
-  /**
-   * TODO: add more tests for:
-   * - links
-   * - images
-   * - headings
-   * - code
-   * - files
-   * - videos
-   * - tables
-   */
+    /**
+     * TODO: add more tests for:
+     * - links
+     * - images
+     * - headings
+     * - code
+     * - files
+     * - videos
+     * - tables
+     */
 });
